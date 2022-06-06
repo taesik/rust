c70f383e703bbeba140c1c78b8dd1a68081d239a
taesik <taesic11@naver.com>
2022-06-06 10:39:37 +0900

    add gameState struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rusty_engine::prelude::*;
+
+
+struct GameState {
+    high_score: u32,
+    current_score : u32,
+    enemy_labels: Vec<String>,
+    spawn_timer:Timer
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score : 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0,false),
+
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+    game.run(GameState::default());
 }
